1ca31fd2d547fe54eeff0b1f4731cc117ecd29b8
vivekmaru36 <vivekmaru036@gmail.com>
2025-07-30 00:48:06 +0530

    hidden files and specify full path to see - or cat < -

---
+++ scripts/level2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# will become a file to open file name "-"
+# reason whjy you can't open is it takes it as stderr/std out need to specify full path
+
+set -euo pipefail
+
+# cat ./-
+# cat < -
+# more -
+# https://linux.die.net/abs-guide/special-chars.html
