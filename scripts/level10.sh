f9b40aee4fff5b54f492ec6a204640a9d27e9630
vivekmaru36 <vivekmaru036@gmail.com>
2025-08-05 20:40:23 +0530

    Revert "level11 done"
    This reverts commit 6389ea302b9bad0137bc29c96e95b859b0110616.

--- scripts/level10.sh
+++ scripts/level10.sh
@@ -2,6 +2,8 @@
 
 # Pr Statement : The password for the next level is stored in the file data.txt in one of the few human-readable strings, preceded by several ‘=’ characters.
 
+
+
 set -eou pipefail
 
 password="4CKMh1JI91bUIZZPXDqGanal4xvAg0JM" # password from level9
@@ -11,13 +13,4 @@ user="bandit9"
 
 echo "Connecting to $host as $user on port $port ...."
 
-# sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p "$port" "$user@$host" 
-# sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p "$port" "$user@$host" "echo 'Passwrd for bandit 9 is : ';cat data.txt | base64 | grep ="
-
-# my approach this is wrong you have to find from the file not convert it to b64
-# cat data.txt | base64 | grep =
-# L47Vsnxp1QuQ+GTzdwLebq0hho1rGVpNOShRzjIaYsoAEPEJr1UY455UPCsSHO4L5+7IaqUCk6c=
-
-sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p "$port" "$user@$host" \
-'strings data.txt | grep "===="'
-# FGUW5ilLVJrxX9kMYMmlN4MgbpfMiqey
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p "$port" "$user@$host" 
