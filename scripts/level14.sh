61cee7327bb61fe900e2c0933dbdffa9d35129f5
vivekmaru36 <vivekmaru036@gmail.com>
2025-08-13 17:55:09 +0530

    private keys version stored in stash

---
+++ scripts/level14.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+password=FO5dwFsc0cbaIiH0h8J2eUks2vdTDwAn
+
+host="bandit.labs.overthewire.org"
+port=2220
+user="bandit13"
+
+# The password for the next level is stored in /etc/bandit_pass/bandit14 and can only be read by user bandit14. For this level, you don’t get the next password, but you get a private SSH key that can be used to log into the next level. Note: localhost is a hostname that refers to the machine you are working on
+
+
+echo "connecting to $host as $user on port $port ..."
+
+# sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p "$port" "$user@$host"
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p "$port" "$user@$host" << 'EOF'
+
+echo "currently the user is bandit13 password is an ssh private keyto login to bandit 14"
+
+echo "runninng below command to take self ssh session to change userr to bandit14 using the private key"
+echo "ssh -i /home/bandi13/sshkey.private bandit14@bandit.labs.overthewire.org -p 2220"
+
+# ssh -i /home/bandit13/sshkey.private bandit14@bandit.labs.overthewire.org -p 2220
+ssh -o StrictHostKeyChecking=no -i /home/bandit13/sshkey.private bandit14@localhost -p 2220
+
+pwd
+ 
+
+echo "Level 13 done"
+EOF
+# sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p "$port" "$user@$host" << 'EOF'
+# set -eou pipefail
+# ls
+# EOF
+
+
+# private key from bandit13 :
+# store this in a file and use that as option -i to log in to servers
+# Pk_14.key has public key stored
+
+# ssh -i ~/Pk_14.key bandit14@bandit.labs.overthewire.org -p 2220
+
+# command for using directly from internet use
+# ssh -i /home/bandi13/sshkey.private bandit14@bandit.labs.overthewire.org -p 2220
+
+# command for using on server level opf bandit's game servers
+# ssh -i /home/bandit13/sshkey.private bandit14@localhost -p 2220   
+
+
+# -----BEGIN RSA PRIVATE KEY-----
+# MIIEpAIBAAKCAQEAxkkOE83W2cOT7IWhFc9aPaaQmQDdgzuXCv+ppZHa++buSkN+
+# gg0tcr7Fw8NLGa5+Uzec2rEg0WmeevB13AIoYp0MZyETq46t+jk9puNwZwIt9XgB
+# ZufGtZEwWbFWw/vVLNwOXBe4UWStGRWzgPpEeSv5Tb1VjLZIBdGphTIK22Amz6Zb
+# ThMsiMnyJafEwJ/T8PQO3myS91vUHEuoOMAzoUID4kN0MEZ3+XahyK0HJVq68KsV
+# ObefXG1vvA3GAJ29kxJaqvRfgYnqZryWN7w3CHjNU4c/2Jkp+n8L0SnxaNA+WYA7
+# jiPyTF0is8uzMlYQ4l1Lzh/8/MpvhCQF8r22dwIDAQABAoIBAQC6dWBjhyEOzjeA
+# J3j/RWmap9M5zfJ/wb2bfidNpwbB8rsJ4sZIDZQ7XuIh4LfygoAQSS+bBw3RXvzE
+# pvJt3SmU8hIDuLsCjL1VnBY5pY7Bju8g8aR/3FyjyNAqx/TLfzlLYfOu7i9Jet67
+# xAh0tONG/u8FB5I3LAI2Vp6OviwvdWeC4nOxCthldpuPKNLA8rmMMVRTKQ+7T2VS
+# nXmwYckKUcUgzoVSpiNZaS0zUDypdpy2+tRH3MQa5kqN1YKjvF8RC47woOYCktsD
+# o3FFpGNFec9Taa3Msy+DfQQhHKZFKIL3bJDONtmrVvtYK40/yeU4aZ/HA2DQzwhe
+# ol1AfiEhAoGBAOnVjosBkm7sblK+n4IEwPxs8sOmhPnTDUy5WGrpSCrXOmsVIBUf
+# laL3ZGLx3xCIwtCnEucB9DvN2HZkupc/h6hTKUYLqXuyLD8njTrbRhLgbC9QrKrS
+# M1F2fSTxVqPtZDlDMwjNR04xHA/fKh8bXXyTMqOHNJTHHNhbh3McdURjAoGBANkU
+# 1hqfnw7+aXncJ9bjysr1ZWbqOE5Nd8AFgfwaKuGTTVX2NsUQnCMWdOp+wFak40JH
+# PKWkJNdBG+ex0H9JNQsTK3X5PBMAS8AfX0GrKeuwKWA6erytVTqjOfLYcdp5+z9s
+# 8DtVCxDuVsM+i4X8UqIGOlvGbtKEVokHPFXP1q/dAoGAcHg5YX7WEehCgCYTzpO+
+# xysX8ScM2qS6xuZ3MqUWAxUWkh7NGZvhe0sGy9iOdANzwKw7mUUFViaCMR/t54W1
+# GC83sOs3D7n5Mj8x3NdO8xFit7dT9a245TvaoYQ7KgmqpSg/ScKCw4c3eiLava+J
+# 3btnJeSIU+8ZXq9XjPRpKwUCgYA7z6LiOQKxNeXH3qHXcnHok855maUj5fJNpPbY
+# iDkyZ8ySF8GlcFsky8Yw6fWCqfG3zDrohJ5l9JmEsBh7SadkwsZhvecQcS9t4vby
+# 9/8X4jS0P8ibfcKS4nBP+dT81kkkg5Z5MohXBORA7VWx+ACohcDEkprsQ+w32xeD
+# qT1EvQKBgQDKm8ws2ByvSUVs9GjTilCajFqLJ0eVYzRPaY6f++Gv/UVfAPV4c+S0
+# kAWpXbv5tbkkzbS0eaLPTKgLzavXtQoTtKwrjpolHKIHUz6Wu+n4abfAIRFubOdN
+# /+aLoRQ0yBDRbdXMsZN/jvY44eM+xRLdRVyMmdPtP8belRi2E2aEzA==
+# -----END RSA PRIVATE KEY-----
