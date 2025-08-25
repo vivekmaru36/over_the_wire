cb57c304eebe0e1ebf64f10c1fa0ebbc342784a0
vivekmaru36 <vivekmaru036@gmail.com>
2025-08-25 20:55:55 +0530

    level 20 done

--- scripts/level20.sh
+++ scripts/level20.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-# There is a setuid binary in the homedirectory that does the following: it makes a connection to localhost on the port you specify as a commandline argument. It then reads a line of text from the connection and compares it to the password in the previous level (bandit20). If the password is correct, it will transmit the password for the next level (bandit21).
-
-# NOTE: Try connecting to your own network daemon to see if it works as you think
+# To gain access to the next level, you should use the setuid binary in the homedirectory. Execute it without arguments to find out how to use it. The password for this level can be found in the usual place (/etc/bandit_pass), after you have used the setuid binary.
 
 host="bandit.labs.overthewire.org"
 port=2220
 user="bandit19"
 password=cGWpMaKXVwDUNgPAVJbWYuGHVn9zl3j8
 
-sshpass -p $password ssh -o StrictHostKeyChecking=no -p $port $user@$host << 'EOF'
+password20=$(sshpass -p $password ssh -o StrictHostKeyChecking=no -p $port $user@$host \ ./bandit20-do cat /etc/bandit_pass/bandit20)
 
-# run nmap scan on local host and give a port number that don't have any service ruinning on it 
-# ss- tlnp
-# 4242 was not in nmap -p0-9999 localhost you can also use ss -tlnp to check the port used port numbers pick anythiong not in ss -tlnp
-
-# nc -l -p 4242 # than run nc -l -p <empty port without a service>     # -l listening -p port 
-
-# open another terminal and run the banditto do script by specifing the port that you started to listen using nc
-
-# use tmux to open a another terminal and please use sleep to put the nc in background 
-
-# start listener in background
-echo "cGWpMaKXVwDUNgPAVJbWYuGHVn9zl3j8" | nc -l -p 4242 &
-# give it a second to start
-sleep 1
-# connect with setuid binary
-./bandit20-do nc localhost 4242
-
-EOF
+echo "Password for bandit20 : $password20"
---
+++ scripts/level21.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# There is a setuid binary in the homedirectory that does the following: it makes a connection to localhost on the port you specify as a commandline argument. It then reads a line of text from the connection and compares it to the password in the previous level (bandit20). If the password is correct, it will transmit the password for the next level (bandit21).
+
+# NOTE: Try connecting to your own network daemon to see if it works as you think
+
+host="bandit.labs.overthewire.org"
+port=2220
+user="bandit19"
+password=cGWpMaKXVwDUNgPAVJbWYuGHVn9zl3j8
+# user="bandit20"
+# password=0qXahG8ZjOVMN9Ghs7iOWsCfZyXOUbYO
+
+# =========================new approach
+# since every pass is stored in etc/bandit_pass but restriceted due to setuiid of that user itself so when you want to give it as input just give it rather than trying to store and such | below approach to give restricted file as input without reading
+
+
+password20=$(sshpass -p $password ssh -o StrictHostKeyChecking=no -p $port $user@$host \ ./bandit20-do cat /etc/bandit_pass/bandit20)
+
+echo "Password for bandit20 : $password20"
+user2="bandit20"
+
+sshpass -p $password20 ssh -o StrictHostKeyChecking=no -p $port $user2@$host
+
+# wow i forget level 19-> 20
+
+# ======================================= experimienting
+# sshpass -p $password ssh -o StrictHostKeyChecking=no -p $port $user@$host << 'EOF'
+
+# # run nmap scan on local host and give a port number that don't have any service ruinning on it 
+# # ss- tlnp
+# # 4242 was not in nmap -p0-9999 localhost you can also use ss -tlnp to check the port used port numbers pick anythiong not in ss -tlnp
+
+# # nc -l -p 4242 # than run nc -l -p <empty port without a service>     # -l listening -p port 
+
+# # open another terminal and run the banditto do script by specifing the port that you started to listen using nc
+
+# # use tmux to open a another terminal and please use sleep to put the nc in background 
+
+# # start listener in background
+# echo "cGWpMaKXVwDUNgPAVJbWYuGHVn9zl3j8" | nc -l -p 4242 &
+# # give it a second to start
+# sleep 1
+# # connect with setuid binary
+# ./bandit20-do nc localhost 4242
+
+# EOF
