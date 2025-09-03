31395b7cca0d18bf9667f3fd00b85485ddc2daa1
vivekmaru36 <vivekmaru036@gmail.com>
2025-09-04 00:30:09 +0530

    level 28 done but unable to automate it

--- scripts/level28.sh
+++ scripts/level28.sh
@@ -4,9 +4,29 @@
 # There is a git repository at ssh://bandit27-git@localhost/home/bandit27-git/repo via the port 2220. The password for the user bandit27-git is the same as for the user bandit27.
 # Clone the repository and find the password for the next level.
 
+
 host="bandit.labs.overthewire.org"
 port=2220
 user="bandit27"
 password="upsNCc7vzaRDx6oZC6GiR6ERwe1MowGB"
 
-sshpass -p "$password" ssh -p $port $user@$host
+# sshpass -p "$password" ssh -p $port $user@$host 
+
+
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p $port $user@$host <<'EOF'
+  mkdir -p /tmp/vivek
+  cd /tmp/vivek
+  git -c core.sshCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
+      clone ssh://bandit27-git@localhost:2220/home/bandit27-git/repo
+  cd repo
+  ls -la
+  cat README
+  cd ..
+  rm -rf repo
+  rmdir /tmp/vivek
+  echo "Hello World"
+EOF
+
+
+# even gpt couldn't help so asking on stack overflow
+# https://stackoverflow.com/staging-ground/79755005
