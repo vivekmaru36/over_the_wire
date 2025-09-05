8d5bf87aa3185b5937ce8bedc62074ea3cca459b
vivekmaru36 <vivekmaru036@gmail.com>
2025-09-05 22:28:24 +0530

    level 30 done i miss automating my scripts

--- scripts/level30.sh
+++ scripts/level30.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# There is a git repository at ssh://bandit30-git@localhost/home/bandit30-git/repo via the port 2220. The password for the user bandit30-git is the same as for the user bandit30.
-
+# There is a git repository at ssh://bandit29-git@localhost/home/bandit29-git/repo via the port 2220. The password for the user bandit29-git is the same as for the user bandit29.
 # Clone the repository and find the password for the next level.
 
 host="bandit.labs.overthewire.org"
@@ -10,3 +9,8 @@ user="bandit29"
 password="4pT1t5DENaYuqnqvadYs1oE4QLCdjmJ7"
 
 sshpass -p "$password" ssh -p $port $user@$host 
+
+
+## credentials
+# - username: bandit30
+# - password: qp30ex3VLz5MDG1n91YowTv4Q8l7CDZL
