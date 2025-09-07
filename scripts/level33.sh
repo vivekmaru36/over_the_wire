596b9415fb51933d54f986e906d0a19188ce93d3
vivekmaru36 <vivekmaru036@gmail.com>
2025-09-07 15:16:13 +0530

    level33.sh done need to fix the exiting conflict arising due to -bash

--- scripts/level33.sh
+++ scripts/level33.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+# Level Goal
+# After all this git stuff, it’s time for another escape. Good luck!
+
+
+host="bandit.labs.overthewire.org"
+port=2220
+user="bandit32"
+password="3O9RfhqyAlVBEZpVb6LYStshZoqoSx5K"
+
+sshpass -p "$password" ssh -p $port $user@$host 
+
+
+# Uppercase Shell
+# $0
+whoami
+# bandit33
+
+# no way i have to call from commands form bin like a monk
+$ /bin/ls
+uppershell
+
+
+$ /bin/cat /etc/bandit_pass/bandit33
+# tQdtbs5D5i2vJwkO8mEyYEyTL8izoeJ0
+
+# ctrl +d
+
