efd5045b08cd82ce45b7fed48aed36291c60f495
vivekmaru36 <vivekmaru036@gmail.com>
2025-10-13 22:12:38 +0530

    natas 11 | still unable to decode the xor using the key in Py

--- scripts/natas/Xor_helper_function.py
+++ scripts/natas/Xor_helper_function.py
@@ -4,16 +4,17 @@ b1 = base64.b64decode("HmYkBwozJw4WNyAAFyB1VUcqOE1JZjUIBis7ABdmbU1GIjEJAyIxTRg="
 b2 = base64.b64decode("eyJzaG93cGFzc3dvcmQiOiJubyIsImJnLWNvbG9yIjoiI2ZmZmZmZiJ9")
 
 # XOR byte by byte
-result = bytes([x ^ y for x, y in zip(b1, b2)])   # i still need to understand this properly
+result = bytes([x ^ y for x, y in zip(b1, b2)])   # now i understand this
+# print(x)
+print(result)  # eDWo eDWo eDWo
 
+
+
+b3=base64.b64decode("eDWo")
+# b4=base64.b64decode('{"showpassword":"yes","bgcolor":"#ffffff"}')   # but this gives no error why ?
+# b4=base64.b64decode('{"showpassword":"no","bgcolor":"#ffffff"}')       # why this gives error god i can't understand
+b4 = b'{"showpassword":"yes","bgcolor":"#ffffff"}'  # directly as bytes
+
+# XOR byte by byte
+result = bytes([x ^ y for x, y in zip(b3, b4)]) 
 print(result)
-
-# new cookie proccess   
-b3=result
-b4={"showpassword":"no","bg-color":"#ffffff"}
-
-
-result2 = bytes([x ^ y for x, y in zip(b3, b4)])   
-
-print(result2)
-
